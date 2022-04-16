f16f3f21487e5a1c672536af95cfef35b24f8d07
ArvikVan <arvikvan@gmail.com>
2022-04-16 20:43:14 +0300

    init

---
+++ src/main/java/arv/natlexservice/NatlexServiceApplication.java
@@ -0,0 +1,13 @@
+package arv.natlexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatlexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatlexServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arv/natlexservice/NatlexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package arv.natlexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatlexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
