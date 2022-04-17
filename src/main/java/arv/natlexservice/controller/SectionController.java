6830fc6f17152d52f9b6b1635a0f97591b52f282
ArvikVan <arvikvan@gmail.com>
2022-04-17 23:18:57 +0300

    crud_sections

--- src/main/java/arv/natlexservice/controller/SectionController.java
+++ src/main/java/arv/natlexservice/controller/SectionController.java
@@ -1,5 +1,6 @@
 package arv.natlexservice.controller;
 
+import arv.natlexservice.controller.exception.NoSectionException;
 import arv.natlexservice.model.Section;
 import arv.natlexservice.repo.SectionRepo;
 import arv.natlexservice.service.SectionServiceImpl;
@@ -21,10 +22,37 @@ public class SectionController {
     @Autowired
     private SectionRepo sectionService;
 
+    @GetMapping("/sections")
+    public List<Section> showAllSections() {
+        List<Section> allSections = sectionService.findAll();
+        return allSections;
+    }
+
     @PostMapping("/sections")
     public Section createSection(@RequestBody Section section) {
         sectionService.save(section);
         return section;
+    }
+
+    @GetMapping("/sections/{id}")
+    public Section getSectionById(@PathVariable Long id) {
+        return sectionService.getById(id);
+    }
+
+    @PutMapping("/sections")
+    public Section updateSection(@RequestBody Section section) {
+        sectionService.save(section);
+        return section;
+    }
+
+    @DeleteMapping("/sections/{id}")
+    public String deleteSection(@PathVariable Long id) {
+        Section section = sectionService.getById(id);
+        if (section == null) {
+            throw new NoSectionException("No section with id " + id + "in db");
+        }
+        sectionService.deleteById(id);
+        return "Section with id " + id + " deleted";
     }
 
 }
---
+++ src/main/java/arv/natlexservice/controller/exception/NoSectionException.java
@@ -0,0 +1,12 @@
+package arv.natlexservice.controller.exception;
+
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 17.04.2022
+ */
+public class NoSectionException extends RuntimeException {
+    public NoSectionException(String message) {
+        super(message);
+    }
+}
